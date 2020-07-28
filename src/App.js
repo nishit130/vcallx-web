aac8ae75c6c6878570b95662f67e0106e4a1b7f3
nis130 <nishitlimbani130@gmail.com>
2020-07-28 18:13:45 +0530

    navi

--- src/App.js
+++ src/App.js
@@ -1,74 +1,68 @@
-import React, { Component } from 'react';
-import './App.css';
-import io from 'socket.io-client';
-import Login from '../src/components/login'
-class App extends Component{
-
-  constructor(props){
+import React, { Component } from "react";
+import "./App.css";
+import io from "socket.io-client";
+import Login from "../src/components/login";
+class App extends Component {
+  constructor(props) {
     super(props);
-    this.localVideoref = React.createRef()
-    this.remoteVideoref = React.createRef()
+    this.localVideoref = React.createRef();
+    this.remoteVideoref = React.createRef();
     this.state = {
-      password_client: '',
-      socket : io(
-        '/webrtcPeer',
-        {
-          part: '/vcallx-web',
-          query: {}
-        }
-       ),
-       login : true,
-    }
-    this.candidates = []
-    this.password = ''
-    this.textref = ''
-    this.caller = ''
-    this.reciver = ''
-      
+      password_client: "",
+      socket: io("/webrtcPeer", {
+        part: "/vcallx-web",
+        query: {},
+      }),
+      login: true,
+    };
+    this.candidates = [];
+    this.password = "";
+    this.textref = "";
+    this.caller = "";
+    this.reciver = "";
   }
-  componentDidMount(){
-    this.state.socket.on('connection-success', success => {
-      console.log("success",success)
-    })
-    this.state.socket.on('offerOrAnswer', (username,sdp) => {
+  componentDidMount() {
+    this.state.socket.on("connection-success", (success) => {
+      console.log("success", success);
+    });
+    this.state.socket.on("offerOrAnswer", (username, sdp) => {
       this.caller = username;
-      alert(username + ' is calling you!')
-      console.log("sdp recived from  ",username);
-      this.pc.setRemoteDescription(new RTCSessionDescription(sdp))
-    })
-    this.state.socket.on('password',(password) => {
-      this.password = password
-    })
-    this.state.socket.on('candidate', (candidate) => {
+      if (this.pc.signalingState === "stable") {
+        alert(username + " is calling you! ");
+      }
+      this.pc.setRemoteDescription(new RTCSessionDescription(sdp));
+    });
+    this.state.socket.on("password", (password) => {
+      this.password = password;
+    });
+    this.state.socket.on("candidate", (candidate) => {
       //this.candidates = [...this.candidates, candidate]
-      this.pc.addIceCandidate(new RTCIceCandidate(candidate))
-    })
-    this.state.socket.on('disconnect-call',(data) => {
-    	console.log('diss call')
+      this.pc.addIceCandidate(new RTCIceCandidate(candidate));
+    });
+    this.state.socket.on("disconnect-call", (data) => {
+      console.log("diss call");
       //this.disconnect();
-    })
+    });
     this.createPc();
   }
-  
+
   handleiceState = (e) => {
-    	console.log('ice state ',this.pc.iceConnectionState)
-    	if(this.pc.iceConnectionState === 'disconnected')
-    	{
-		this.disconnect();
-		this.sendToPeer('disconnect-call','');
-	}
-}
-   
+    console.log("ice state ", this.pc.iceConnectionState);
+    if (this.pc.iceConnectionState === "disconnected") {
+      this.disconnect();
+      this.sendToPeer("disconnect-call", "");
+    }
+  };
 
   handleICEcandidateEvent = (e) => {
-    if(e.candidate){
-      this.sendToPeer('candidate', e.candidate)
+    if (e.candidate) {
+      this.sendToPeer("candidate", e.candidate);
     }
-  }
+  };
 
   handleTrackEvent = (e) => {
-    this.remoteVideoref.current.srcObject = e.streams[0]
-  }
+    this.remoteVideoref.current.srcObject = e.streams[0];
+  };
 
   // handleNegotiationNeededEvent = () => {
   //   this.pc.createOffer().then((offer) => {
@@ -79,117 +73,113 @@ class App extends Component{
   // }
   createPc = () => {
     const pc_config = {
-      "iceServers": [
+      iceServers: [
         {
-          urls : 'stun:stun.l.google.com:19302'
+          urls: "stun:stun.l.google.com:19302",
         },
         {
-          urls: 'turn:numb.viagenie.ca',
+          urls: "turn:numb.viagenie.ca",
           credential: process.env.REACT_APP_TURN_CREDINTIAL,
-          username: process.env.REACT_APP_TURN_USERNAME
+          username: process.env.REACT_APP_TURN_USERNAME,
         },
-      ]
-    }
-    this.pc = new RTCPeerConnection(pc_config)
+      ],
+    };
+    this.pc = new RTCPeerConnection(pc_config);
     this.pc.onicecandidate = this.handleICEcandidateEvent;
     this.pc.ontrack = this.handleTrackEvent;
     this.pc.oniceconnectionstatechange = this.handleiceState;
     this.setLocalVideo();
-  }
-
-
-
-
-
-
-  makeid(length) {
-    var result           = '';
-    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    var charactersLength = characters.length;
-    for ( var i = 0; i < length; i++ ) {
-       result += characters.charAt(Math.floor(Math.random() * charactersLength));
-    }
-    return result;
-  }
+  };
   setLocalVideo = () => {
     const constraints = {
-      video : true,
+      video: true,
       //audio: true,
-    }
-    navigator.mediaDevices.getUserMedia(constraints).then((stream) =>{
-      window.localStream = stream
-      this.localVideoref.current.srcObject = stream
-      this.pc.addStream(stream)
-    },(streamError) => {
-      console.log("Stream Error: ",streamError)
-    })
-  }
-  sendToPeer = (messageType, payload,username) => {
-    if(messageType === 'offerOrAnswer')
-    {
-      console.log('reciver:',this.reciver);
-      this.state.socket.emit(messageType,{
-        username:username,
-        socketID : this.state.socket.id,
-        payload,
-      })
-    }
-    else
-    {
-      console.log('offer wala nahi hai ye')
+    };
+    navigator.mediaDevices.getUserMedia(constraints).then(
+      (stream) => {
+        window.localStream = stream;
+        this.localVideoref.current.srcObject = stream;
+        this.pc.addStream(stream);
+      },
+      (streamError) => {
+        console.log("Stream Error: ", streamError);
+      }
+    );
+  };
+  sendToPeer = (messageType, payload, username) => {
+    if (messageType === "offerOrAnswer") {
+      console.log("reciver:", this.reciver);
       this.state.socket.emit(messageType, {
-        socketID : this.state.socket.id,
-        payload
-      })
+        username: username,
+        socketID: this.state.socket.id,
+        payload,
+      });
+    } else {
+      console.log("offer wala nahi hai ye");
+      this.state.socket.emit(messageType, {
+        socketID: this.state.socket.id,
+        payload,
+      });
     }
-  }
-  
+  };
+
   LoginToogle = (value) => {
-      this.setState({
-        login : value,
-      })
-  }
+    this.setState({
+      login: value,
+    });
+  };
   createOffer = () => {
-    console.log('offer')
-    if(!this.pc)
-    {
+    console.log("offer");
+    if (!this.pc) {
       this.createPc();
     }
-    this.pc.createOffer({offerToReceiveVideo:1,iceRestart: true}).then(sdp => {
-      // console.log(JSON.stringify(sdp))
-      this.pc.setLocalDescription(sdp).then(() => console.log("local descp added"))
-      this.sendToPeer('offerOrAnswer',sdp,this.reciver)
-      
-      //this.sendToPeer('password',this.state.password_client)
-    })
-  }
+    this.pc
+      .createOffer({ offerToReceiveVideo: 1, iceRestart: true })
+      .then((sdp) => {
+        // console.log(JSON.stringify(sdp))
+        this.pc
+          .setLocalDescription(sdp)
+          .then(() => console.log("local descp added"));
+        this.sendToPeer("offerOrAnswer", sdp, this.reciver);
+
+        //this.sendToPeer('password',this.state.password_client)
+      });
+  };
 
   createAnswer = () => {
-    //console.log("caller pass: ",this.password)
-    if(!this.pc)
-    {
+    if (!this.pc) {
       this.createPc();
     }
-    console.log("Answer")
-    this.pc.createAnswer({offerToReceiveVideo: 1, offerToReceiveAudio: 1,iceRestart:true}).then(sdp => {
-      // console.log(JSON.stringify(sdp)
-      this.sendToPeer('offerOrAnswer', sdp,this.caller)
-      this.pc.setLocalDescription(sdp).then(this.sendToPeer('accepted-call',''))
-    })
-  }
+    console.log("Answer");
+    this.pc
+      .createAnswer({
+        offerToReceiveVideo: 1,
+        offerToReceiveAudio: 1,
+        iceRestart: true,
+      })
+      .then((sdp) => {
+        // console.log(JSON.stringify(sdp)
+        this.sendToPeer("offerOrAnswer", sdp, this.caller);
+        this.pc
+          .setLocalDescription(sdp)
+          .then(this.sendToPeer("accepted-call", ""));
+      });
+  };
   disconnect = () => {
     // this.sendToPeer('disconnect-call','')
-    if(this.localVideoref.current.srcObject)
-    {
-      this.localVideoref.current.srcObject.getTracks().forEach(track => track.stop())
-      this.localVideoref.current.srcObject = null
+    if (this.localVideoref.current.srcObject) {
+      this.localVideoref.current.srcObject
+        .getTracks()
+        .forEach((track) => track.stop());
+      this.localVideoref.current.srcObject = null;
     }
-    if(this.remoteVideoref.current.srcObject) {
-      this.remoteVideoref.current.srcObject.getTracks().forEach(track => track.stop())
-      this.remoteVideoref.current.srcObject = null
+    if (this.remoteVideoref.current.srcObject) {
+      this.remoteVideoref.current.srcObject
+        .getTracks()
+        .forEach((track) => track.stop());
+      this.remoteVideoref.current.srcObject = null;
     }
-    if(this.pc)
-    {
+    if (this.pc) {
       this.pc.ontrack = null;
       this.pc.onicecandidate = null;
       this.pc.close();
@@ -198,8 +188,8 @@ class App extends Component{
     this.candidates = [];
     // this.setLocalVideo();
     this.createPc();
-  }
-  
+  };
+
   // addCandidate = () => {
   //   // const candidate = JSON.parse(this.textref.value)
   //   // console.log('Adding candidate:', candidate)
@@ -209,43 +199,59 @@ class App extends Component{
   //   })
   // }
 
-  render(){
-      
-        if(this.state.login)
-        {
-          return (
-          <Login socket={this.state.socket} onLogin={this.LoginToogle}/>
-          );
-        }
-        else
-        {
-          return (
-          <view className="page">
-            <div className="heading">
-              <h1>Vcallx Video Call</h1>
+  render() {
+    if (this.state.login) {
+      return <Login socket={this.state.socket} onLogin={this.LoginToogle} />;
+    } else {
+      return (
+        <view className='page'>
+          <div className='heading'>
+            <h1>Vcallx Video Call</h1>
+          </div>
+
+          <div className='videoView'>
+            <div className=''>
+              <video className='localVideo' ref={this.localVideoref} autoPlay />
             </div>
-            
-          <div className="videoView">
-            <div className="">
-              <video className="localVideo" ref={this.localVideoref} autoPlay />
-            </div>
-            <div className="">
-              <video className="remoteVideo" ref={this.remoteVideoref} autoPlay />
+            <div className=''>
+              <video
+                className='remoteVideo'
+                ref={this.remoteVideoref}
+                autoPlay
+              />
             </div>
           </div>
-          <p id="password">{this.state.password_client}</p>
-          <input className="inputArea" placeholder="Enter Username" onChange={(event) => {this.reciver = event.target.value}}/>
+          <p id='password'>{this.state.password_client}</p>
+          <input
+            className='inputArea'
+            placeholder='Enter Username'
+            onChange={(event) => {
+              this.reciver = event.target.value;
+            }}
+          />
           {/* <input className="inputArea" placeholder="Enter Video Call ID" onChange={(event) => {this.textref = event.target.value}}/> */}
           <div>
-            <button className="btn" onClick={this.createOffer}>Make Call</button>
-            <button className="btn" onClick={this.createAnswer}>Connect to Call</button>
-            <button className="btn" onClick={() => {this.disconnect(); this.sendToPeer('disconnect-call','')}}>diconnect to Call</button>
+            <button className='btn' onClick={this.createOffer}>
+              Make Call
+            </button>
+            <button className='btn' onClick={this.createAnswer}>
+              Connect to Call
+            </button>
+            <button
+              className='btn'
+              onClick={() => {
+                this.disconnect();
+                this.sendToPeer("disconnect-call", "");
+              }}
+            >
+              diconnect to Call
+            </button>
           </div>
           {/* <button onClick={this.setRemoteDescription}>set Remote Description</button>
           <button onClick={this.addCandidate}>Add candidate</button> */}
-          </view>
-          );
-        }
+        </view>
+      );
+    }
   }
 }
 
--- src/components/login.js
+++ src/components/login.js
@@ -1,154 +1,107 @@
-import React, { Component } from 'react';
-import { Button, Form, FormGroup, Label, Input, Fade  } from 'reactstrap';
-import _ from 'lodash';
-import Signup from './signup';
- 
-const validationMethods =  {
-    required : (field, value) => {
-        if (!value.toString().trim().length) {
-            return  `This ${field} field is required.`
-        }
-    },
-    isEmail: (field,value) => {
-               
-    }
-} 
- 
-const validateForm = (form) => {
-    const loginForm = document.getElementById(form)
-     return loginForm.querySelectorAll('[validations]');
-}
- 
-const runValidationRules  = (element, errors) => {
-    const target = element;
-    const field =  target.name;
-    const value = target.value
-    let validations =  element.getAttribute('validations');
-    validations =  validations.split(',')
- 
-    for (let validation of validations) {
-        validation = validation.split(':');
-        const rule = validation[0];
-        const error = validationMethods[rule](field, value);
-        errors[field] = errors[field] || {};
-        if(error) {
-            errors[field][rule] = error;
-        } else {
-            if(_.isEmpty(errors[field])) {
-                delete errors[field];
-            } else {
-                delete errors[field][rule];
-            }
-        }
-    }
- 
-    return errors;
-}
- 
- 
+import React, { Component } from "react";
+// import { Button, Form, FormGroup, Label, Input, Fade } from "reactstrap";
+import _ from "lodash";
+import Signup from "./signup";
+import "../login.css";
+
 export default class Login extends Component {
- 
-    constructor(props) {
-        super(props);
-        this.state = {
-            email: '',
-            password: '',
-            errors: []
-        }
-    }
- 
- 
-    componentDidMount(){
-        this.props.socket.on('check-user',(message, value) => {
-            if(value)
-            {
-                this.props.onLogin(false);
-                alert(message);
-            }
-            else
-            {
-                alert(message);
-               // this.props.socket.emit('addUser', {socketID:this.props.socket.id,username: email, password: password});
-            }
-        })
-    }
-    login = (event) => {
- 
-        event.preventDefault();
- 
-        const formElements = validateForm("loginForm");
- 
-        formElements.forEach(element=> {
-           const errors = runValidationRules(element, this.state.errors);
-            this.setState({
-                errors: errors
-            });
-        })
- 
-        const email = this.state.email;
-        const password = this.state.password;
-        const errors =  this.state.errors;
-        console.log(email, password, errors);
-        //console.log(this.props)
-        this.props.socket.emit('check-user', {socketID:this.props.socket.id,username: email, password: password});
-       
-    }
- 
-    handleChange = (event) => {
-        const target = event.target;
-        const field =  target.name;
-        const value = target.value
- 
-        const errors = runValidationRules(target, this.state.errors);
- 
-        this.setState({
-            errors: errors
-        });
- 
-        this.setState({
-            [field]:  value
-        });
-    }
-    render() {
-        return (
-            <div className="container">
-                <Form id="loginForm" method="post" onSubmit={this.login}>
-                    <FormGroup>
-                        <Label for="email">Email</Label>
-                        <Input
-                            type="text"
-                            validations={['required','isEmail']}
-                            name="email"
-                            value={this.state.email}
-                            onChange={this.handleChange}
-                            id="email"
-                            placeholder="Enter your email address."
-                        />
-                      <FromValidationError field={this.state.errors.email} />
-                    </FormGroup>
-                    <FormGroup>
-                        <Label for="password">Password</Label>
-                        <Input
-                            type="password"
-                            validations={['required']}
-                            name="password"
-                            value={this.state.password}
-                            onChange={this.handleChange}
-                            id="password"
-                            placeholder="Enter your password."
-                        />
-                        <FromValidationError field={this.state.errors.password} />
-                    </FormGroup>
-                    <Button>login</Button>
-                </Form>
-                <Signup onLogin={this.props.onLogin} socket={this.props.socket} />
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: "",
+      errors: [],
+      Signup: false,
+    };
+  }
+
+  componentDidMount() {
+    this.props.socket.on("check-user", (message, value) => {
+      if (value) {
+        this.props.onLogin(false);
+      } else {
+        alert(message);
+        // this.props.socket.emit('addUser', {socketID:this.props.socket.id,username: email, password: password});
+      }
+    });
+  }
+  submit = () => {
+    const email = this.state.email;
+    const password = this.state.password;
+    const errors = this.state.errors;
+    console.log(email, password, errors);
+    //console.log(this.props)
+    this.props.socket.emit("check-user", {
+      socketID: this.props.socket.id,
+      username: email,
+      password: password,
+    });
+  };
+
+  handleChange = (event) => {
+    const target = event.target;
+    const field = target.name;
+    const value = target.value;
+
+    this.setState({
+      [field]: value,
+    });
+  };
+  login = () => {
+    this.setState({
+      Signup: false,
+    });
+  };
+  Signup = () => {
+    this.setState({
+      Signup: true,
+    });
+  };
+  render() {
+    if (this.state.Signup) {
+      return (
+        <Signup
+          login={this.login}
+          onLogin={this.props.onLogin}
+          socket={this.props.socket}
+        />
+      );
+    } else {
+      return (
+        <div className='page'>
+          <h2 className='heading'>Login</h2>
+          <div id='loginForm'>
+            <div>
+              <input
+                type='text'
+                name='email'
+                value={this.state.email}
+                onChange={this.handleChange}
+                id='email'
+                className='inputArea'
+                placeholder='Enter your email address.'
+              />
             </div>
-        );
+            <div>
+              <input
+                type='password'
+                name='password'
+                value={this.state.password}
+                onChange={this.handleChange}
+                id='password'
+                className='inputArea'
+                placeholder='Enter your password.'
+              />
+            </div>
+            <button onClick={this.submit} className='btn'>
+              login
+            </button>
+          </div>
+          <p style={{ color: "white" }} onClick={this.Signup}>
+            Create account?
+          </p>
+        </div>
+      );
     }
+  }
 }
- 
-const FromValidationError = props => (
-    <Fade in={Boolean(props.field)}  tag="p" className="error">
-       { props.field ?  Object.values(props.field).shift() : '' } 
-  </Fade>
-);
